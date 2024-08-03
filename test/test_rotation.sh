2b5e7b1ec0bdfdfcdbc682c73e40c0808b8e24ab
omerk160 <omerkanias160@gmail.com>
2024-08-03 10:43:10 +0300

    tst 4

--- test/test_rotation.sh
+++ test/test_rotation.sh
@@ -11,7 +11,7 @@ echo $OLD_KEYS
 echo -e "\n\nCopying the rotation script into your public instance."
 echo -e "Command: scp ssh_keys_rotation.sh ubuntu@$PUBLIC_IP:/home/ubuntu/\n\n"
 
-scp -i /home/ubuntu/github_test_ssh_key ssh_keys_rotation.sh ubuntu@$PUBLIC_IP:/home/ubuntu/
+scp ssh_keys_rotation.sh ubuntu@$PUBLIC_IP:/home/ubuntu/
 
 echo -e "\n\nConnecting to your public instance and executing the rotation script."
 echo -e "Command: ssh -i $KEY_PATH ubuntu@$PUBLIC_IP \"./ssh_keys_rotation.sh $PRIVATE_IP\"\n\n"
